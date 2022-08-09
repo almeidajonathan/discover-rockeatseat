becdfa9306c02f692532b55dc3d4e25225e324d7
Jonathan Almeida <almeidajonathaan@gmail.com>
2022-08-09 00:02:51 -0300

    exemplo de encadeamento de promises usando fetch

--- especializar/javascript-async-promises/promiseFetch.js
+++ especializar/javascript-async-promises/promiseFetch.js
@@ -2,4 +2,5 @@ fetch("https://api.github.com/users/almeidajonathan")
   .then((response) => response.json())
   .then((data) => fetch(data.repos_url))
   .then((res) => res.json())
-  .then((d) => console.log(d));
+  .then((d) => console.log(d))
+  .catch((err) => console.log(err))
