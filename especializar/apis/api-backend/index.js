2d99da94f2b033bc1356d2f2cf567dcc691e079a
Jonathan Almeida <almeidajonathaan@gmail.com>
2022-08-14 23:43:33 -0300

    add rotas

--- especializar/apis/api-backend/index.js
+++ especializar/apis/api-backend/index.js
@@ -1,6 +1,25 @@
 const http = require('http')
 const express = require('express')
 
+
 const app = express()
 
+let author = 'Jonathan'
+
+
 app.listen('5001')
+
+// middleware
+app.use(express.json())
+
+// rotas
+app.route('/').get((req, res) => res.send(author))
+app.route('/').post((req, res) => res.send(req.body))
+app.route('/').put((req, res) => {
+    author = req.body.author
+    console.log(author);
+    res.send(author)
+    })
+app.route('/:id').delete((req, res) => {
+    res.send(req.params.id)
+})
