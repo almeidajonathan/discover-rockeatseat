1128cfc63e75d9c4b73b8992e16b6aafb9bd2d38
Jonathan Almeida <almeidajonathaan@gmail.com>
2022-10-10 23:48:46 -0300

    add validação no form

--- especializar/dom/projeto-modal/script.js
+++ especializar/dom/projeto-modal/script.js
@@ -4,6 +4,7 @@ const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.close');
 const submitBtn = document.querySelector('button.confirm');
 const input = document.querySelector('input');
+const warningMessage = document.querySelector('.warning');
 
 
 function toggleModal() {
@@ -11,15 +12,25 @@ function toggleModal() {
 }
 
 function submitInput() {
-    const warningMessage = document.querySelector('.warning');
-    if(input.value == ' ') {
+
+    input.onkeyup = function(event) {
+        // console.log(event.key);
+        warningMessage.classList.add('disabled');
+    }
+
+    if(input.value == '') {
         console.log('removendo classe disabled');
         warningMessage.classList.remove('disabled');
-    } else if(input.value != ' ' && warningMessage.classList[1] == 'disabled') {
-        warningMessage.classList.add('disabled');
-        console.log('adicionando classe disabled');
+    // } else if(input.value != '' && warningMessage.classList[1] != 'disabled') {
+    //     warningMessage.classList.add('disabled');
+    //     console.log('adicionando classe disabled');
+    } else {
 
-    } 
+        modal.classList.add('disabled');
+    }
+    
+    input.value = '';
+   
 }
 
 //LISTENERS
